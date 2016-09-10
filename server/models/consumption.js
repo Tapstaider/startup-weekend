a9218f981e683d06bd65e940abfc5c8ec7b3210d
Krarr <averlant.nicolas@gmail.com>
2016-09-10 21:41:13 +0200

    [dev_server] Création du modèle de données pour l'entité consumption

---
+++ server/models/consumption.js
@@ -0,0 +1,33 @@
+// Création du modèle de la table Consumption.
+
+var Consumption = F.sequelize.define('consumption', {
+  id: {
+    type: F.Sequelize.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  id_user: {
+    type: F.Sequelize.BIGINT,
+    allowNull: false,
+    references: 'user',
+    referencesKey: 'id'
+  },
+  id_drink_rfid: {
+    type: F.Sequelize.BIGINT,
+    allowNull: false,
+    references: 'drink_rfid',
+    referencesKey: 'id'
+  },
+  weight: {
+    type: F.Sequelize.FLOAT
+  }
+}, {
+  freezeTableName: true
+});
+
+Consumption.sync().then(function(){
+  // Table Crée ou mise à jour
+  exports.name = 'consumption';
+  exports.Schema = Consumption;
+});
