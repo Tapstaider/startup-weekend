6c270ae5bd70c0d08a2669a18e5c5ec5ebbf6e0e
Krarr <averlant.nicolas@gmail.com>
2016-09-10 21:09:20 +0200

    [dev_server] Création du modèle de données pour l'entité user

---
+++ server/models/user.js
@@ -0,0 +1,38 @@
+// Création du modèle de la table User.
+
+var User = F.sequelize.define('user', {
+  id: {
+    type: F.Sequelize.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: F.Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: F.Sequelize.STRING,
+    allowNull: false
+  },
+  weight: {
+    type: F.Sequelize.FLOAT,
+    allowNull: false
+  },
+  tall: {
+    type: F.Sequelize.FLOAT,
+    allowNull: false
+  },
+  password: {
+    type: F.Sequelize.STRING,
+    allowNull: false
+  }
+}, {
+  freezeTableName: true
+});
+
+User.sync().then(function(){
+  // Table crée ou mise à jour
+  exports.name = 'user';
+  exports.Schema = User;
+});
