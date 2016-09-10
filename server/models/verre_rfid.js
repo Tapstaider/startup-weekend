0385a72a6bc48395edbf93665711490acf3213fb
Krarr <averlant.nicolas@gmail.com>
2016-09-10 21:26:25 +0200

    [dev_server] Création du modèle de données pour l'entité drink_rfid

---
+++ server/models/verre_rfid.js
@@ -0,0 +1,33 @@
+// Création du modèle de la table Drink_rfid.
+
+var Drink_rfid = F.sequelize.define('drink_rfid', {
+  id: {
+    type: F.Sequelize.BIGINT,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  rfid: {
+    type: F.Sequelize.STRING,
+    allowNull: false
+  },
+  id_beer: {
+    type: F.Sequelize.BIGINT,
+    allowNull: false,
+    references: 'beer',
+    referencesKey: 'id'
+  },
+  capacity: {
+    type: F.Sequelize.FLOAT,
+    allowNull: false,
+    comment: "Capacity of the drink in cL"
+  }
+}, {
+  freezeTableName : true
+});
+
+Drink_rfid.sync().then(function() {
+  // Table crée ou mise à jour
+  exports.name = 'drink_rfid';
+  exports.Schema = Drink_rfid;
+});
