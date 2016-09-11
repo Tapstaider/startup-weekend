7933ae707eaafcdbb0ecb5a269e0b9126afae101
Johann Lecocq <git-jo@johann-lecocq.fr>
2016-09-11 16:55:33 +0200

    finalisation du code

--- server/public/app/conso/controller.js
+++ server/public/app/conso/controller.js
@@ -3,5 +3,22 @@
 var app=angular.module('app');
 
 app.controller("ConsoController",["$scope","ConsoService",function($scope,ConsoService){
-	$scope.bieres={"Triple Karmeliett":166,"1664":122,"Maredsou":55};
+	$scope.bieres={};//{"Tripel Karmeliet":166,"1664":122,"Maredsous":55};
+	ConsoService.getConsumptions().then(function(response){
+		var conso=response.data.consumptions;
+		console.log(conso);
+		for(var i=0;i<conso.length;i++){
+			ConsoService.getMyBeers(conso[i].id_drink_rfid).then(function(response){
+				if(!$scope.bieres.hasOwnProperty(response.data.beer.name)){
+					$scope.bieres[response.data.beer.name]=0;
+				}
+				$scope.bieres[response.data.beer.name]=$scope.bieres[response.data.beer.name]+1;
+			},function(data){
+			
+			});
+		}
+		
+	},function(data){
+	
+	});
 }]);
--- server/public/app/prix/service.js
+++ server/public/app/prix/service.js
@@ -5,6 +5,6 @@ var app=angular.module('app');
 app.service("PrixService",["$http",function($http){
 	this.getBeers=function(){
 		return $http.get("http://localhost:8000/beers");
-	}
+	};
 }]);
 
