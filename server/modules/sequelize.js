39be3aaf3ee2314f17f105fa4e59e28589f3b22d
Krarr <averlant.nicolas@gmail.com>
2016-09-11 12:41:09 +0200

    [dev_server] Change sequelizeModule for supporting sqlite connections.

--- server/modules/sequelize.js
+++ server/modules/sequelize.js
@@ -10,18 +10,20 @@ exports.install = function(){
 
   var database_name_option = 'database.name';
   var username_database_option = 'database.username';
-  var passeword_database_option = 'database.passeword';
+  var password_database_option = 'database.password';
   var host_database_option = 'database.host';
   var dialect_database_option = 'database.dialect';
   var pool_database_option = 'database.pool';
+  var storage_path_database_option = 'database.storage.path';
 
   // Récupération des options depuis les fichiers de configurations de Total.
   var database_name = CONFIG(database_name_option);
   var username_database = CONFIG(username_database_option);
-  var passeword_database = CONFIG(passeword_database_option);
+  var password_database = CONFIG(password_database_option);
   var host_database = CONFIG(host_database_option);
   var dialect_database = CONFIG(dialect_database_option);
-  var pool_database = CONFIG(pool_database);
+  var pool_database = CONFIG(pool_database_option);
+  var storage_path_database = CONFIG(storage_path_database_option);
 
   // Connection à la base de données grâce à Sequelize.
   var Sequelize = require('sequelize');
@@ -29,6 +31,7 @@ exports.install = function(){
   F.sequelize = new Sequelize(database_name, username_database, passeword_database, {
     host: host_database,
     dialect: dialect_database,
-    pool : pool_database
+    pool : pool_database,
+    storage: storage_path_database
   });
 }
