315da999539c29d5eeb62c13e3d9eeb09b2ea1b1
Johann Lecocq <git-jo@johann-lecocq.fr>
2016-09-11 15:11:34 +0200

    integration des templates

--- server/public/app/conso/controller.js
+++ server/public/app/conso/controller.js
@@ -3,5 +3,5 @@
 var app=angular.module('app');
 
 app.controller("ConsoController",["$scope","ConsoService",function($scope,ConsoService){
-
+	$scope.bieres={"Triple Karmeliett":166,"1664":122,"Maredsou":55};
 }]);
--- server/public/app/prix/controller.js
+++ server/public/app/prix/controller.js
@@ -3,6 +3,12 @@
 var app=angular.module('app');
 
 app.controller("PrixController",["$scope","PrixService",function($scope,PrixService){
-
+	$scope.bieres={"Triple Karmeliett":166,"1664":122,"Maredsou":55};
+	$scope.incrementeBiere=function(nomBiere){
+		console.log(nomBiere+"+1");
+	};
+	$scope.decrementeBiere=function(nomBiere){
+		console.log(nomBiere+"-1");
+	};
 }]);
 
